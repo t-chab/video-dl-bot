c6224c41503cbf0032c12a98292da5d06d561512
Thomas Chabaud <tchabaud@chabs.name>
2018-03-10 22:19:01 +0100

    Add gif and mp3 features.

--- main.py
+++ main.py
@@ -12,11 +12,11 @@ import glob
 import logging
 import os
 import sys
-import urllib
+import urllib.request
 import uuid
 
 import pycountry as pycountry
-import tenacity
+import validators
 import youtube_dl
 from telegram.ext import Updater, CommandHandler, run_async
 
@@ -47,25 +47,45 @@ logger = logging.getLogger(__name__)
 
 def show_help(bot, update):
     """Send a message when the command /help is issued."""
-    update.message.reply_text('Usage /dl [url]')
+    update.message.reply_text("""Available commands : 
+                                 - Download video : /dl [url]
+                                 - Download video using a proxy to bypass geo-restriction : /dlp [url]
+                                 - Update proxy : /proxy [country_code]
+                                 - Make gif from video : /gif [url]
+                                 - Get mp3 from video : /mp3 [url]
+                                 """)
 
 
 @run_async
 def download(bot, update, args):
     logger.info('%s - args: "%s"', sys._getframe().f_code.co_name, args)
+    telegram_chat = update.message.chat_id
     url = args[0]
-    file_prefix = VIDEO_DL_DIR + VIDEO_FILE_PREFIX
-    encoded_chat_id = bytes(str(update.message.chat_id), 'ascii')
-    video_file = file_prefix + base64.urlsafe_b64encode(encoded_chat_id).decode('ascii') \
-                 + '_' \
-                 + str(uuid.uuid4()) \
-                 + '.mp4'
-    try:
-        ydl_opts = ytdl_config(video_file, dl_proxy='')
-        ytdl_download(url, video_file=video_file, ydl_opts=ydl_opts)
-    except Exception as e:
-        logger.fatal('Download failed: %s, retrying with proxy ...', e)
-        ytdl_with_proxy(url, video_file)
+    download_url(bot, telegram_chat, url)
+
+
+@run_async
+def download_with_proxy(bot, update, args):
+    logger.info('%s - args: "%s"', sys._getframe().f_code.co_name, args)
+    telegram_chat = update.message.chat_id
+    url = args[0]
+    download_url(bot, telegram_chat, url, with_proxy=True)
+
+
+@run_async
+def gif(bot, update, args):
+    logger.info('%s - args: "%s"', sys._getframe().f_code.co_name, args)
+    telegram_chat = update.message.chat_id
+    url = args[0]
+    download_url(bot, telegram_chat, url, output_gif=True)
+
+
+@run_async
+def mp3(bot, update, args):
+    logger.info('%s - args: "%s"', sys._getframe().f_code.co_name, args)
+    telegram_chat = update.message.chat_id
+    url = args[0]
+    download_url(bot, telegram_chat, url, output_type='mp3')
 
 
 @run_async
@@ -76,22 +96,45 @@ def proxy(bot, update, args):
                                     + PROXY_WS_HOST
                                     + ':' + str(PROXY_WS_PORT)
                                     + '/update?country=' + country.alpha_2).read()
-    bot.sendMessage(chat_id=update.message.chat_id, text=str(result))
+    bot.sendMessage(chat_id=update.message.chat_id, text=result.decode('utf-8'))
 
 
-def get_proxy_country_code():
+def download_url(bot, telegram_chat, url, output_gif=False, output_type='mp4', with_proxy=False):
+    if not validators.url(url):
+        bot.sendMessage(chat_id=telegram_chat, text='Please, stop sending shit.')
+        return
+    current_proxy = ''
+    if with_proxy:
+        current_proxy = get_proxy()
+        logger.info('Proxy selected: %s', current_proxy)
+    file_prefix = VIDEO_DL_DIR + VIDEO_FILE_PREFIX
+    encoded_chat_id = bytes(str(telegram_chat), 'ascii')
+    video_file = file_prefix + base64.urlsafe_b64encode(encoded_chat_id).decode('ascii') \
+                 + '_' \
+                 + str(uuid.uuid4())
+    gif_ext = ''
+    if output_gif:
+        gif_ext = '.output_gif'
+    video_file = video_file + gif_ext + '.' + output_type
+    output_mp3 = (output_type == 'mp3')
+    try:
+        ydl_opts = ytdl_config(video_file, is_gif=output_gif,
+                               is_mp3=output_mp3, dl_proxy=current_proxy)
+        ytdl_download(url, video_file=video_file, ydl_opts=ydl_opts)
+    except Exception as e:
+        proxy_info = ''
+        if with_proxy:
+            proxy_info = 'through proxy ' + str(current_proxy)
+        logger.fatal('Download %s failed: %s', proxy_info, e)
+        error_msg = 'Sorry, download failed due to error : ' + str(e)
+        bot.sendMessage(chat_id=telegram_chat, text=error_msg)
+
+
+def get_proxy():
     return urllib.request.urlopen('http://'
                                   + PROXY_WS_HOST
                                   + ':' + str(PROXY_WS_PORT)
                                   + '/').read()
-
-
-@tenacity.retry
-def ytdl_with_proxy(url, video_file):
-    country_code = get_proxy_country_code()
-    logger.info('New proxy selected: %s', country_code)
-    ydl_opts = ytdl_config(video_file, dl_proxy=country_code)
-    ytdl_download(url, video_file, ydl_opts)
 
 
 def ytdl_download(url, video_file, ydl_opts):
@@ -104,7 +147,18 @@ def ytdl_download(url, video_file, ydl_opts):
         logger.info('Video file renamed to "%s"', final_name)
 
 
-def ytdl_config(video_file, dl_proxy=''):
+def ytdl_config(video_file, is_gif=False, is_mp3=False, dl_proxy=''):
+    ffmpeg_cmd = "ffmpeg -i {} -y -vcodec libx264 -crf 23 -vprofile baseline " \
+                 + "-b:v 500k -maxrate 500k -bufsize 800k -vf scale=-2:480 -level 3.0 " \
+                 + "-threads 0 -pix_fmt yuv420p -codec:a aac -ac 2 -strict experimental " \
+                 + "-ab 128k -movflags +faststart " + video_file
+    if is_gif:
+        ffmpeg_cmd = "ffmpeg -i {} -y -vcodec libx264 -crf 23 -vprofile baseline " \
+                     + "-b:v 500k -maxrate 500k -bufsize 800k -vf scale=-2:480 -level 3.0 " \
+                     + "-threads 0 -pix_fmt yuv420p -an -strict experimental " \
+                     + "-movflags +faststart " + video_file
+    if is_mp3:
+        ffmpeg_cmd = "ffmpeg -i {} -y -vn -ac 2 -f mp3 " + video_file
     ydl_opts = {
         'format': 'bestvideo+bestaudio/best',
         'outtmpl': VIDEO_DL_DIR + '%(id)s',
@@ -114,10 +168,7 @@ def ytdl_config(video_file, dl_proxy=''):
             'key': 'ExecAfterDownload',
             # see https://ffmpeg.org/ffmpeg-filters.html#scale
             # for explanation on -2 choice for scale filter
-            'exec_cmd': "ffmpeg -i {} -y -vcodec libx264 -crf 23 -vprofile baseline "
-                        "-b:v 500k -maxrate 500k -bufsize 800k -vf scale=-2:480 -level 3.0 "
-                        "-threads 0 -pix_fmt yuv420p -codec:a aac -ac 2 -strict experimental "
-                        "-ab 128k -movflags +faststart " + video_file
+            'exec_cmd': ffmpeg_cmd
         }],
         'addmetadata': True,
         'xattrs': True,
@@ -138,14 +189,17 @@ def get_finished_name(filename):
 
 def send_file(bot, job):
     # check if there is files to send
-    path = VIDEO_DL_DIR + VIDEO_FILE_PREFIX + '*_' + FINISHED_PATTERN + '*.mp4'
+    path = VIDEO_DL_DIR + VIDEO_FILE_PREFIX + '*_' + FINISHED_PATTERN + '*.mp?'
     logger.debug('Checking for new files to upload from %s', path)
     for file in glob.glob(path):
         encoded_chat = file.split('_')[1]
         chat = int(base64.b64decode(encoded_chat).decode('ascii'))
         try:
             logger.info('Uploading "%s" to chat "%s"', file, chat)
-            bot.sendVideo(chat_id=chat, video=open(file, 'rb'), timeout=UPLOAD_TIMEOUT)
+            if file.endswith('.mp3'):
+                bot.sendAudio(chat_id=chat, audio=open(file, 'rb'), timeout=UPLOAD_TIMEOUT)
+            else:
+                bot.sendVideo(chat_id=chat, video=open(file, 'rb'), timeout=UPLOAD_TIMEOUT)
             logger.info('File "%s" sent successfully !', file)
             clean(file)
         except Exception as e:
@@ -184,7 +238,10 @@ def main():
     # Map commands
     dp.add_handler(CommandHandler("help", show_help))
     dp.add_handler(CommandHandler("dl", download, pass_args=True))
+    dp.add_handler(CommandHandler("dlp", download_with_proxy, pass_args=True))
     dp.add_handler(CommandHandler("proxy", proxy, pass_args=True))
+    dp.add_handler(CommandHandler("gif", gif, pass_args=True))
+    dp.add_handler(CommandHandler("mp3", mp3, pass_args=True))
 
     # log all errors
     dp.add_error_handler(error)
